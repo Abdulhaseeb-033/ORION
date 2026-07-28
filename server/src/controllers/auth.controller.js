e2664129dcfdd60a7c5d304d3a1665d1990822db
Abdulhaseeb 033 <ahaseebans110@gmail.com>
2026-07-28 19:27:09 +0500

    Complete authentication system with register and login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,8 +1,14 @@
-import { registerUser } from "../services/auth.service.js";
+import { registerUser, loginUser } from "../services/auth.service.js";
 
 export const register = async(req, res) => {
     console.log(req.body)
     const result = await registerUser(req.body);
+
+    res.status(201).json(result);
+};
+
+export const login = async (req, res) => {
+    const result = await loginUser(req.body);
 
     res.status(201).json(result);
 };
--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,3 +1,6 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 
 export const registerUser = async (userData) => {
     const { fullName, username, email, password } = userData;
@@ -5,9 +8,72 @@ export const registerUser = async (userData) => {
     if (!fullName || !username || !email || !password) {
         throw new Error("All fields are required.");
     }
+  
+    const existingUser = await User.findOne({
+        $or: [
+        {email},
+        {username}
+    ]
+});
+
+if (existingUser) {
+    throw new Error("User already exists.");
+}
+
+const hashedPassword = await bcrypt.hash(password, 10);
+
+const user = await User.create({
+    fullName,
+    username,
+    email,
+    password:hashedPassword
+});
+
+return {
+    success: true,
+    message: "User registered successfully.",
+    user,
+};
+
+};
+
+export const loginUser= async (userData) => {
+    const { email, password } = userData;
+
+    if (!email || !password) {
+        throw new Error("Email and Password are required.");
+    }
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+        throw new Error("User not found.");
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if(!isPasswordCorrect) {
+        throw new Error("Invalid credentials.");
+    }
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "7d",
+        }
+    );
 
     return {
         success: true,
-        message: "Validation Passed"
+        message: "Login successful.",
+        token,
+        user,
     };
-};
+}
